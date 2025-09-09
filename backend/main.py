482df907685852c55dfc9fd082268fe5c60bcf20
Eduardo1865 <eduardo.a@gec.inatel.br>
2025-09-09 17:33:33 -0300

    Adiciona dependências FastAPI e configurações do projeto

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,6 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/')
-def index():
-    return {'data':'blog list'}
-
-@app.get('/blog/{id}')
-def about(id:int):
-    # achando o a largura do
-    return {'data': id}
-
-@app.get('/blog/unpublished')
+def main():
+    print("Hello from backend!")
 
 
-@app.get('/blog/{id}/comments')
-def comments(id):
-    #dando um fetch nos comentarios com id = id
-    return {'data': {'1','2'}}
+if __name__ == "__main__":
+    main()
